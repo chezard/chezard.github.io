ded9f3cbe1b048d608a77d00d3376363bb800f4f
chezard <subhanmasimov02@gmail.com>
2025-01-13 18:22:42 +0300

    initial

---
+++ assets/js/5e95c892.c6a07fe9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkchezard_github_io=self.webpackChunkchezard_github_io||[]).push([[9647],{8122:(e,r,s)=>{s.r(r),s.d(r,{default:()=>d});s(6540);var u=s(4164),a=s(1082),c=s(204),h=s(2831),t=s(1410),i=s(4848);function d(e){return(0,i.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,i.jsx)(t.A,{children:(0,h.v)(e.route.routes)})})}}}]);
